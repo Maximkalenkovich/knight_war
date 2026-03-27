250e93d9eb556a48ddbdea3469fba6ac8d2f7598
MaximKalenkovich1998 <peterson.maxim@mail.ru>
2026-03-27 19:32:27 +0300

    first commit: Unity 2D project (knight_war)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            inputVector.y = 1f;
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            inputVector.y = -1f;
+        }
+
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            inputVector.x = -1f;
+        }
+
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            inputVector.x = 1f;
+        }
+
+        Debug.Log(inputVector);
+
+    }
+}
